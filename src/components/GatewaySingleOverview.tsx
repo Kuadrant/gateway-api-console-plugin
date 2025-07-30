b4c854cd3814673113166a08efb3851ee5118170
R-Lawton <rlawton@redhat.com>
2025-07-30 11:01:45 +0100

    Adding attached httproutes to gateway view
    Signed-off-by: R-Lawton <rlawton@redhat.com>

--- src/components/GatewaySingleOverview.tsx
+++ src/components/GatewaySingleOverview.tsx
@@ -3,7 +3,6 @@ import Helmet from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import { PageSection, Title } from '@patternfly/react-core';
 import { useLocation } from 'react-router-dom';
-import GatewayTabProvider from './GatewayTabProvider';
 import {
   useK8sWatchResources,
   K8sResourceCommon,
@@ -11,6 +10,7 @@ import {
 } from '@openshift-console/dynamic-plugin-sdk';
 
 import extractResourceNameFromURL from '../utils/nameFromPath';
+import AttachedResources from './AttachedResources';
 
 const GatewayPoliciesPage: React.FC = () => {
   const { t } = useTranslation('plugin__gateway-api-console-plugin');
@@ -34,25 +34,24 @@ const GatewayPoliciesPage: React.FC = () => {
   const watchedResources = useK8sWatchResources<{ gateway: K8sResourceCommon }>(resources);
   const { loaded, loadError, data: httpRoute } = watchedResources.gateway;
 
-  console.log('HHTPROUTE', httpRoute);
-
   return (
     <>
       <Helmet>
-        <title data-test="example-page-title">{t('Gateway Console Plugin')}</title>
+        <title data-test="example-page-title">{t('Kuadrant Policies')}</title>
       </Helmet>
       <PageSection hasBodyWrapper={false}>
-        <Title headingLevel="h2">{t('Attached Resources')}</Title>
+        <Title headingLevel="h2">{t('Kuadrant Policies')}</Title>
         {!loaded ? (
           <div>Loading...</div>
         ) : loadError ? (
           <div>Error loading Gateway: {loadError.message}</div>
         ) : (
-          <GatewayTabProvider resource={httpRoute} />
+          <AttachedResources resource={httpRoute} />
         )}
       </PageSection>
     </>
   );
 };
+
 
 export default GatewayPoliciesPage;
